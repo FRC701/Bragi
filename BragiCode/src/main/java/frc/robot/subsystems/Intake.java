618e25342976d75f8212304325eff5f20ac5a051
Daija Canegan <daijac@vandenrobotics.com>
2024-01-18 16:55:43 -0800

    made command and smart dashboard

--- BragiCode/src/main/java/frc/robot/subsystems/Intake.java
+++ BragiCode/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,17 +4,19 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-
   public TalonFX IntakeM1;
   public TalonFX IntakeM2;
   public TalonFX IntakeM3;
   public IntakeState mIntakeState;
+
 
   /** Creates a new Intake. */
 
@@ -28,6 +30,10 @@ public class Intake extends SubsystemBase {
     IntakeM2 = new TalonFX(Constants.IntakeConstants.IntakeMotor2);
     IntakeM3 = new TalonFX(Constants.IntakeConstants.IntakeMotor3);
 
+  var slot0Configs = new Slot0Configs();
+    slot0Configs.kP = 0.11;
+    IntakeM3.getConfigurator().apply(slot0Configs, 0.050); 
+
     mIntakeState = IntakeState.S_WaitingForBall;
   }
 
@@ -37,7 +43,7 @@ public class Intake extends SubsystemBase {
         WaitingForBall();
         break;
       case S_PopOutIntake:
-
+        PopOutIntake();
         break;
       case S_WaitingForShooter:
         WaitingForShooter();
@@ -48,25 +54,30 @@ public class Intake extends SubsystemBase {
   public void WaitingForBall() {
     IntakeM1.set(0.1);
     IntakeM2.set(0.1);
-   ]
+
   }
 
   public void PopOutIntake() {
-    IntakeM3.set(0.8);
-      if (IntakeM1.getFault_ForwardHardLimit().getValue()) {
+    IntakeM3.setPosition(0);
+    if (IntakeM1.getFault_ForwardHardLimit().getValue()) {
       mIntakeState = IntakeState.S_WaitingForShooter;
-    }    
+    }
   }
 
   public void WaitingForShooter() {
     IntakeM1.set(0);
     IntakeM2.set(0);
-
   }
 
+  public void Position(){
+    SmartDashboard.putNumber("IntakeMotor1Position",IntakeM1.getPosition().getValueAsDouble());
+    SmartDashboard.putNumber("IntakeMotor1Position",IntakeM2.getPosition().getValueAsDouble());
+  }
   @Override
   public void periodic() {
     RunIntakeState();
+    Position();
+
     // This method will be called once per scheduler run
   }
 }
