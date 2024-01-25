c4ef60bb58332e964ec6cbad9c637369626869f0
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2024-01-24 23:24:28 -0800

    fix code bugs in Constants file

--- BragiCode/src/main/java/frc/robot/subsystems/Intake.java
+++ BragiCode/src/main/java/frc/robot/subsystems/Intake.java
@@ -9,7 +9,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.;
 
 public class Intake extends SubsystemBase {
   public TalonFX IntakeMotorRight;
