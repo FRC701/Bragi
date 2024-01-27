844999b4018f60a5d45e58f3f4df7f6b9d6d4878
Khoa Tran <koaht@vandenrobotics.com>
2024-01-27 13:23:36 -0800

    Updated PID

--- BragiCode/src/main/java/frc/robot/TunerConstants.java
+++ BragiCode/src/main/java/frc/robot/TunerConstants.java
@@ -14,11 +14,11 @@ public class TunerConstants {
   // The steer motor uses any SwerveModule.SteerRequestType control request with the
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains =
-      new Slot0Configs().withKP(0.14294).withKI(0).withKD(0.13225).withKS(0).withKV(1.5).withKA(0);
+      new Slot0Configs().withKP(0.18).withKI(0).withKD(0.13225).withKS(0).withKV(1.26367).withKA(0);
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(0.2).withKI(0).withKD(0).withKS(0).withKV(0).withKA(0);
+      new Slot0Configs().withKP(0).withKI(0).withKD(0).withKS(0).withKV(0).withKA(0);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
