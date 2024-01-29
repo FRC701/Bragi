6ea1d2cac8e1cc85c231241a369a2696a971aec7
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2024-01-29 01:15:36 -0800

    debugging shootersubsystem

--- BragiCode/src/main/java/frc/robot/RobotContainer.java
+++ BragiCode/src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.Test;
 
 /**
@@ -17,8 +18,71 @@ import frc.robot.subsystems.Test;
 public class RobotContainer {
   // navx
   private final Test test = new Test();
+  private final ShooterSubsystem ShooterSubsystem = new ShooterSubsystem();
+  // The robot's subsystems and commands are defined here...
+  private double MaxSpeed = 6; // 6 meters per second desired top speed 6
+  private double MaxAngularRate =
+      1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity 1.5 * pi
 
-  public RobotContainer() {}
+  // private final CommandJoystick joystick =
+  //  new CommandJoystick(Constants.OperatorConstants.kDriverControllerPort);
+  //  private final CommandXboxController CODriver =
+  //    new CommandXboxController(Constants.OperatorConstants.kCoDriverControllerPort);
+  // private final ShooterSubsystem mShooter = new ShooterSubsystem();
+
+  // My joystick
+  // private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
+
+  // private final Trigger TriggerJoystick = new Trigger(joystick.button(2));
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  // private final SwerveRequest.FieldCentric drive =
+  //  new SwerveRequest.FieldCentric()
+  //    .withDeadband(MaxSpeed * 0.1)
+  //  .withRotationalDeadband(MaxAngularRate * 0.28) // Add a 10% deadband
+  //          .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+
+  // driving in open loop
+  // private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  // private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  // private final Telemetry logger = new Telemetry(MaxSpeed);
+  private void configureBindings() {
+    // CODriver.x().onTrue(new GetShooterVelocity(mShooter));
+
+    //  drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+    //    drivetrain.applyRequest(
+    //    () ->
+    //          drive
+    //          .withVelocityX(-joystick.getY() * 0.25 * MaxSpeed) // Drive forward with
+    // negative Y (forward)
+    //        .withVelocityY(
+    //          -joystick.getX() * 0.25 * MaxSpeed) // Drive left with negative X (left)
+    //    .withRotationalRate(
+    //    -joystick.getTwist()
+    //          * MaxAngularRate) // Drive counterclockwise with negative X (left)
+    // ));
+
+    // CODriver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    // CODriver.b()
+    //  .whileTrue(
+    //    drivetrain.applyRequest(
+    //      () ->
+    //        point.withModuleDirection(new Rotation2d(-joystick.getY(), -joystick.getX()))));
+
+    // reset the field-centric heading on left bumper press
+    // TriggerJoystick.onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    // if (Utils.isSimulation()) {
+    // drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+    // }
+    //  drivetrain.registerTelemetry(logger::telemeterize);
+  }
+
+  public RobotContainer() {
+    configureBindings();
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
