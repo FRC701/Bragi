b45c0a23ec27772a45bdba6a6159eaea0dd5830b
Rheanne <rheanneb@vandenrobotics.com>
2024-02-14 15:26:05 -0800

    Math done

--- BragiCode/src/main/java/frc/robot/VisionTable.java
+++ BragiCode/src/main/java/frc/robot/VisionTable.java
@@ -4,24 +4,42 @@
 
 package frc.robot;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Distance;
 import java.time.temporal.ValueRange;
 
 /** Add your docs here. */
 public class VisionTable {
+  public static final double CameraHeight = 123456789;
+  public static final double SpeakerHeight = 987654321;
+
   public int[][] DataTable = {
-    {1, 0, 2},
-    {5, 50, 2},
-    {10, 100, 2}
+    {1, 0},
+    {5, 50},
+    {10, 100}
   };
 
-  // {target, rpm, distance}
+  // {distance, rpm)
 
-  public double[] GetBound(double TA) {
-    int iTa = (int) TA;
+  public double GetSpeed(double Distance) {
+    double Slope =
+        (GetBound(Distance)[1][1] - GetBound(Distance)[0][1])
+            / (GetBound(Distance)[1][0] - GetBound(Distance)[0][0]);
+    double Speed = Slope * (Distance - GetBound(Distance)[0][0]) + GetBound(Distance)[0][1];
+    return Speed;
+  }
+
+  public double GetAngle(double Distance) {
+    double Angle = Math.tan((SpeakerHeight - CameraHeight) / Distance);
+    return Angle;
+  }
+
+  public double[][] GetBound(double Distance) {
+    int iTa = (int) Distance;
     ValueRange range;
     int i = 0;
     if (iTa > DataTable[DataTable.length][0] || iTa < DataTable[0][0]) {
-      double[] outlier = {0, 0};
+      double[][] outlier = {{0, 0}, {0, 0}};
       return outlier;
     } else {
       for (; i < DataTable.length; i++) {
@@ -30,7 +48,9 @@ public class VisionTable {
           break;
         }
       }
-      double[] bound = {DataTable[i][1], DataTable[i + 1][1]};
+      double[][] bound = {
+        {DataTable[i][0], DataTable[i][1]}, {DataTable[i + 1][0], DataTable[i + 1][1]}
+      };
       return bound;
     }
   }
