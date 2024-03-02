138114b61005d206c0419acc0e08d865ac01722f
Khoa Tran <koaht@vandenrobotics.com>
2024-03-02 13:31:16 -0800

    Update Odometry in CommandSwerveDrivetrain

--- BragiCode/src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ BragiCode/src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -5,11 +5,22 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import frc.robot.subsystems.VisionSubsystem;
+
+
+import java.util.Optional;
 import java.util.function.Supplier;
+
+import org.photonvision.EstimatedRobotPose;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem so it can be used
@@ -19,6 +30,19 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
   private static final double kSimLoopPeriod = 0.005; // 5 ms
   private Notifier m_simNotifier = null;
   private double m_lastSimTime;
+
+  private VisionSubsystem m_vision = new VisionSubsystem();
+
+   private final SwerveDrivePoseEstimator m_poseEstimator = new SwerveDrivePoseEstimator(
+      m_kinematics,
+      this.getPigeon2().getRotation2d(), // NWU
+      new SwerveModulePosition[] {
+          this.getModule(0).getPosition(true),
+          this.getModule(1).getPosition(true),
+          this.getModule(2).getPosition(true),
+          this.getModule(3).getPosition(true)
+      },
+      new Pose2d());
 
   public CommandSwerveDrivetrain(
       SwerveDrivetrainConstants driveTrainConstants,
@@ -57,5 +81,37 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
               updateSimState(deltaTime, RobotController.getBatteryVoltage());
             });
     m_simNotifier.startPeriodic(kSimLoopPeriod);
+  }
+
+  public void updateOdometry() {
+    m_poseEstimator.update(
+        this.getPigeon2().getRotation2d(), // NWU
+      new SwerveModulePosition[] {
+          this.getModule(0).getPosition(true),
+          this.getModule(1).getPosition(true),
+          this.getModule(2).getPosition(true),
+          this.getModule(3).getPosition(true)
+      });
+    // Pose2d newEstimate = m_poseEstimator.getEstimatedPosition();
+    // fieldPub.set(new double[] {
+    //     newEstimate.getX(),
+    //     newEstimate.getY(),
+    //     newEstimate.getRotation().getDegrees()
+    // });
+
+    // Also apply vision measurements. We use 0.3 seconds in the past as an example
+    // -- on a real robot, this must be calculated based either on latency or
+    // timestamps.
+
+    // m_poseEstimator.addVisionMeasurement(
+    // ExampleGlobalMeasurementSensor.getEstimatedGlobalPose(
+    // m_poseEstimator.getEstimatedPosition()),
+    // Timer.getFPGATimestamp() - 0.3);
+    Optional<EstimatedRobotPose> GlobalVisionPose = m_vision.getEstimatedGlobalPose();
+    if(GlobalVisionPose.isPresent()){
+      Pose2d pose2d = GlobalVisionPose.get().estimatedPose.toPose2d();
+      m_poseEstimator.addVisionMeasurement(pose2d, GlobalVisionPose.get().timestampSeconds - 0.3);
+    }
+
   }
 }
